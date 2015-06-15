0d38acfa34713bcbd0e499d413c52bbf49d8d15e
Omer Gertel <omerg@infinidat.com>
2015-06-15 14:10:25 +0300

    Fix ephemeral behaviour

--- chords/more/ephemeral.py
+++ chords/more/ephemeral.py
@@ -1,19 +1,15 @@
 from ..pool import Pool
 
-class EphemearlPool(Pool):
+class EphemeralPool(Pool):
     """
-    Sometimes it's useful to create a resource out of thin air, that isn't stored anywhere
-    This often happens when resources have dependencies that are hard to describe using the request semantics,
-    but could also help when some condition needs to be met.
+    Sometimes it's useful to create a resource out of thin air, that isn't pinned to a concrete resource.
+    This might happen when resources have dependencies that are hard to describe using the request semantics,
+    like a counter, a bundle of resources or a slice of a list.
     
-    A few examples:
-     - incrementing a counter
-     - taking a slice of a list
-    
+    If the ephemeral resource requires another resource, you can override can_acquire, acquire and release to also take other resources.
     """
     def create(self, request):
         raise NotImplementedError()
 
     def find(self, request):
-        while True:
-            yield self.create(request)
+        yield self.create(request)
